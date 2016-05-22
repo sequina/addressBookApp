e0190f510e53ec1e2ec657be692b39d500266495
sequina <sccaro89@gmail.com>
2016-05-21 19:30:55 -0500

    navbar added with links

---
+++ app/controllers/NavCtrl.js
@@ -0,0 +1,16 @@
+// app.controller("NavCtrl", function ($scope) {
+//   $scope.navThings =[
+//     {
+//       name: "Logout",
+//       url:'#/logout'
+//     },
+//     {
+//       name:"Contacts",
+//       url:'#/items/all'
+//     },
+//     {
+//       name:"New Contact",
+//       url:'#/items/new'
+//     }
+//   ];
+// })
