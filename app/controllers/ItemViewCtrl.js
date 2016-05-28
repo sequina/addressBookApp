20c227ec5596a8f495ead155ac57e649fe27fcb0
sequina <sccaro89@gmail.com>
2016-05-28 17:51:12 -0500

    nav is not displaying

--- app/controllers/ItemViewCtrl.js
+++ app/controllers/ItemViewCtrl.js
@@ -1,13 +1,13 @@
 app.controller("ItemViewCtrl", function($scope, $routeParams, itemStorage) {
   $scope.contacts = [];
-  $scope.selectItem = {};
+  $scope.selectedItem = {};
   console.log($routeParams.contactId);
 
   itemStorage.getContactList().then(function (itemCollection) {
     console.log("itemCollection from promise", itemCollection);
     $scope.contacts = itemCollection;
 
-    $scope.selectItem = $scope.contacts.filter(function(contacts) {
+    $scope.selectedItem = $scope.contacts.filter(function(contacts) {
       return contacts.id === $routeParams.contactsId;
     })[0];
   });
